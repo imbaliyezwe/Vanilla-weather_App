239c784f5f94d4983fb674d596b18e3d8e1f2b3b
nomkitha <119540549+nomkitha@users.noreply.github.com>
2023-04-03 09:11:37 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -46,10 +46,11 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-function search(city) {
+function getForecast(coordinates) {
   let apiKey = "2ff29bed3181c3526c35cc5408037f85";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayTemperature);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 }
 
 function handleSubmit(event) {
